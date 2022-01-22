055e491c02bacbf133280831f8b0b5459802764e
Kai Ho CHAN <Crashes.John@gmail.com>
2022-01-23 06:31:43 +0800

    function update
    - shorthands
    - queue check
    - youtube account

--- cogs/music.py
+++ cogs/music.py
@@ -5,11 +5,15 @@ from discord.channel import VoiceChannel
 from discord.ext import commands
 import youtube_dl
 
+# import json file, youtube email and password inside
+import json
+with open('setting.json', mode='r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
 # globol variable
 # queue for multimedia flow control
 queue = []; ctr00 = -1
 v_title_pe = None; v_url_pe = None; v_uploader_pe = None; request_pe = None
-
 
 # multimedia related function
 class music(commands.Cog):
@@ -39,6 +43,11 @@ class music(commands.Cog):
                 ctx.voice_client.play(source_q, after=lambda x=None: self.load_queue(ctx=ctx))
                 self.client.dispatch("queue_load", ctx)
     
+    # command &&j, lazy version of &&skip
+    @commands.command()
+    async def j(self, ctx):
+        await ctx.invoke(self.client.get_command('join'))
+
     # command &&join, join/change voice channel
     @commands.command()
     async def join(self, ctx):
@@ -56,6 +65,11 @@ class music(commands.Cog):
             embed.description = f'➡️ Changed channel from `{ctx.voice_client.channel}` to `{voice_ch}`'
             await ctx.send(embed=embed)
 
+    # command &&ds, lazy version of &&discon
+    @commands.command()
+    async def ds(self, ctx):
+        await ctx.invoke(self.client.get_command('discon'))
+
     # command &&discon, leave voice channel
     @commands.command()
     async def discon(self, ctx):
@@ -63,6 +77,11 @@ class music(commands.Cog):
         embed = discord.Embed(description='🛄 Leave channel to find backpack')
         await ctx.send(embed=embed)
     
+    # command &&p url, lazy version of &&play url
+    @commands.command()
+    async def p(self, ctx, url):
+        await ctx.invoke(self.client.get_command('play'), url=url)
+
     # command &&play url, play youtube/youtube-dl supported video
     @commands.command()
     async def play(self, ctx, url):
@@ -76,8 +95,8 @@ class music(commands.Cog):
             await ctx.invoke(self.join)
 
         FFMPEG_OPT = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        YDL_OPT = {'format': "bestaudio"}
-        v_client = ctx.voice_client         
+        YDL_OPT = {'format': "bestaudio", 'username': jdata['YT_EMAIL'], 'password': jdata['YT_PW']}
+        v_client = ctx.voice_client
         with youtube_dl.YoutubeDL(YDL_OPT) as ytDL:
             info = ytDL.extract_info(url, download=False)
             v_title = info.get('title', None)
@@ -89,7 +108,8 @@ class music(commands.Cog):
         if v_client.is_playing():
             ctr00 += 1
             queue.append([source, v_title, v_url, v_uploader, ctx.author.mention])
-            embed2 = discord.Embed(description=f'Added to queue, position {ctr00+1}, link: {url}')
+            embed2 = discord.Embed(title=f'Added to queue, position {ctr00+1}')
+            embed2.description = f'[{v_title} by {v_uploader}]({url})\r\nQueued by [{ctx.author.mention}]'
             await ctx.send(embed=embed2)
             return
         elif not(v_client.is_playing()):
@@ -105,6 +125,11 @@ class music(commands.Cog):
         embed.url = v_url_q
         embed.description = f'Queued by [{request_q}]'
         await ctx.send(embed=embed)
+
+    # command &&fs, lazy version of &&skip
+    @commands.command()
+    async def fs(self, ctx):
+        await ctx.invoke(self.client.get_command('skip'))
 
     # command &&skip, skip song and load next song
     @commands.command()
@@ -141,6 +166,26 @@ class music(commands.Cog):
         ctx.voice_client.resume()
         embed = discord.Embed(description="▶️ Resumed")
         await ctx.send(embed=embed)
+
+    # command &&q, lazy version of &&queue
+    @commands.command()
+    async def q(self, ctx):
+        await ctx.invoke(self.client.get_command('queue'))
+
+    # command &&queue, check songs in queue
+    @commands.command()
+    async def queue(self, ctx):
+        global ctr00
+        embed = discord.Embed()
+        if ctr00 == -1:
+            embed.description = 'Queue is empty!'
+            await ctx.send(embed=embed)
+        elif ctr00 != -1:
+            output = '```CSS\r\n[Songs in queue]\r\n'
+            for i in range(ctr00+1):
+                output += f'{i+1}: {queue[i][1]}\r\n'
+            output += '```'
+            await ctx.send(output)
 
 def setup(nbot):
     nbot.add_cog(music(nbot))
